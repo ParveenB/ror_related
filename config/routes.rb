6e4aa3d8712a0a811b46b3a0316f9ce8d20ba2ae
bigc <bigc@bigc.(none)>
2014-05-12 11:45:07 +0530

    Simple_Form Part 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,5 @@
 GemExpo::Application.routes.draw do
+  resources :login_pages
+  root "login_pages#index"  #come to http://localhost:3000/
+  match ':controller(/:action(/:id))', :via => [:get,:post]
 end
